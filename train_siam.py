a3d3d99b3e1933a71987a385129de98eac6af0a4
James Emilian <dream.jamesemi@gmail.com>
2024-04-25 11:54:41 -0400

    Update train_siam.py

--- train_siam.py
+++ train_siam.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import os
 import random
+from matplotlib import pyplot as plt
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
@@ -12,13 +13,15 @@ from models.backbone.vit import ViT
 from models.head.topdown_heatmap_simple_head import TopdownHeatmapSimpleHead
 from models.losses import JointsMSELoss
 from torch.optim import AdamW
-from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import StepLR 
+from torch.optim.lr_scheduler import ReduceLROnPlateau 
 from PIL import Image
 from itertools import combinations, product
 from torch.utils.data import Dataset
 from torch.nn.utils import clip_grad_norm_
 import datetime
 import numpy as np
+import seaborn as sns
 
 def seed_everything(seed_value = 42):
     random.seed(seed_value)
@@ -251,92 +254,134 @@ def contrastive_loss(output1, output2, label, margin=1.0):
                       label * torch.pow(torch.clamp(margin - euclidean_distance, min=0.0), 2))
     return loss
 
-def train_model(model, mlp_model, train_loader, val_loader, device, patience):
-    # criterion = JointsMSELoss().to(device)
-    criterion_bce = nn.BCEWithLogitsLoss()
-    criterion_contrastive = ContrastiveLoss()
-    # criterion_cosine = nn.CosineEmbeddingLoss(margin=0.5).to(device)
-    optimizer = AdamW(model.parameters(), lr=1e-4, betas=(0.9, 0.999), weight_decay=0.01)
-    scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
+
+def plot_losses(train_losses, val_losses):
+    sns.set(style="whitegrid")  # Set the style of the plot using seaborn
+    plt.figure(figsize=(10, 5))  # Set the size of the plot
+    plt.title('Training and Validation Loss')  # Set the title of the plot
+
+    # Plot training and validation loss
+    sns.lineplot(x=range(1, len(train_losses)+1), y=train_losses, label='Training Loss', linewidth=2.5)
+    sns.lineplot(x=range(1, len(val_losses)+1), y=val_losses, label='Validation Loss', linewidth=2.5)
+
+    plt.xlabel('Epochs')  # Label for the x-axis
+    plt.ylabel('Loss')  # Label for the y-axis
+    plt.legend()  # Add a legend
+    plt.tight_layout()  # Automatically adjust subplot parameters to give specified padding
+    
+    plt.savefig('training_validation_loss.png')  # Save the plot to a file
+    plt.show()  # Display the plot
+
+def train_model(model, mlp_model, train_loader, val_loader, device, patience, lr, num_epochs):
+    criterion_contrastive = ContrastiveLoss().to(device)
+    optimizer = AdamW(model.parameters(), lr=lr, betas=(0.9, 0.999), weight_decay=0.01)
+    # scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
+    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=1, verbose=True)
     scaler = GradScaler()
-    epochs_without_improvement, min_loss = 0, 1e8
+
+    epochs_without_improvement, min_loss = 0, float('inf')
+    train_losses, val_losses = [], []
 
     model.train()
     mlp_model.train()
-    for epoch in range(200):  # Number of epochs
+    for epoch in range(num_epochs):  # Adjust the number of epochs if needed
         total_loss, val_loss = 0, 0
         for images, targets in tqdm(train_loader):
-
             images = [image.to(device) for image in images]
             targets = targets.to(device)
-            batch_size = targets.size(0)            
-            targets_cosine = torch.where(targets == 1, 1, -1)  # Assuming binary targets: 1 for similar, -1 for dissimilar
 
             optimizer.zero_grad()
-
             with autocast():
                 output1 = model(images[0])
                 output2 = model(images[1])
-                output = torch.reshape(torch.cat((output1, output2), dim=1), (batch_size, -1))
-                final_output = mlp_model(output)
-                # loss = criterion_bce(final_output, targets) + 1e-4*criterion_contrastive(output1.view(-1), output2.view(-1), targets)
-                loss = criterion_contrastive(output1.view(output1.shape[0], -1), output2.view(output1.shape[0], -1), targets)
+                loss = criterion_contrastive(output1.view(output1.shape[0], -1), output2.view(output2.shape[0], -1), targets)
 
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
 
             total_loss += loss.item()
-         
-        scheduler.step()
 
+        avg_train_loss = total_loss / len(train_loader)
+        train_losses.append(avg_train_loss)
+
+        val_loss = 0
         model.eval()
         mlp_model.eval()
-        val_loss = 0.0
         with torch.no_grad():
             for images, targets in tqdm(val_loader):
                 images = [image.to(device) for image in images]
                 targets = targets.to(device)
-                targets_cosine = torch.where(targets == 1, 1, -1)  # Assuming binary targets: 1 for similar, -1 for dissimilar
                 output1 = model(images[0])
                 output2 = model(images[1])
-                output = torch.reshape(torch.cat((output1, output2), dim=1), (targets.size(0), -1))
-                final_output = mlp_model(output)
-                # loss = criterion_bce(final_output, targets) + criterion_contrastive(output1.view(-1), output2.view(-1), targets)
-                loss = criterion_contrastive(output1.view(output1.shape[0], -1), output2.view(output1.shape[0], -1), targets)
-                # loss = criterion_cosine(output1, output2, targets_cosine)
-                val_loss += loss.item()
+                loss = criterion_contrastive(output1.view(output1.shape[0], -1), output2.view(output2.shape[0], -1), targets)
                 
-            avg_val_loss = val_loss / len(val_loader)
+                val_loss += loss.item()
 
-        print(f'Epoch {epoch+1}, Train Loss: {total_loss/len(train_loader)}, Val Loss : {avg_val_loss}')
+        avg_val_loss = val_loss / len(val_loader)
+        val_losses.append(avg_val_loss)
+        scheduler.step(avg_val_loss)
+        
+        print(f'Epoch {epoch+1}, Train Loss: {avg_train_loss}, Val Loss : {avg_val_loss}')
 
         if avg_val_loss < min_loss:
+            min_loss = avg_val_loss
             epochs_without_improvement = 0
-            torch.save(model, '/home/nthumbav/Downloads/ViTPose_pytorch/vitpose_ckpt.pth')
-            torch.save(mlp_model, '/home/nthumbav/Downloads/ViTPose_pytorch/mlp_ckpt.pth')
-        
+            # torch.save(model.state_dict(), f'vitpose_checkpoint_epoch_{epoch+1}.pth')
+            # torch.save(mlp_model.state_dict(), f'mlp_checkpoint_epoch_{epoch+1}.pth')\
+            checkpoint_filename = f'vitpose_lr{lr}_epoch{epoch+1}_valloss{avg_val_loss:.4f}.pth'
+            torch.save(model.state_dict(), checkpoint_filename)
+            torch.save(mlp_model.state_dict(), checkpoint_filename.replace("vitpose", "mlp"))
         else:
             epochs_without_improvement += 1
             if epochs_without_improvement >= patience:
                 print('Early stopping activated')
                 break
 
+    return train_losses, val_losses
 
+def plot_losses(train_losses, val_losses, lr, num_epochs):
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(10, 5))
+    sns.lineplot(x=range(1, len(train_losses)+1), y=train_losses, label='Training Loss', linewidth=2.5)
+    sns.lineplot(x=range(1, len(val_losses)+1), y=val_losses, label='Validation Loss', linewidth=2.5)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title(f'Training and Validation Loss (LR={lr}, Epochs={num_epochs})')
+    plt.legend()
+    plot_filename = f'training_validation_loss_lr{lr}_epochs{num_epochs}.png'
+    plt.savefig(plot_filename)
+    plt.show()
 
 
 def test_model(model, mlp_model, test_loader, device):
     model.eval()
     mlp_model.eval()
+    pos_distances, neg_distances = [], [] 
     with torch.no_grad():
         for images, targets in tqdm(test_loader):
             images = [image.to(device) for image in images]
             targets = targets.to(device)
             output1 = model(images[0])
             output2 = model(images[1])
-            output = torch.reshape(torch.cat((output1, output2), dim=1), (targets.size(0), -1))
-            final_output = mlp_model(output)
-            
+            # output = torch.reshape(torch.cat((output1, output2), dim=1), (targets.size(0), -1))
+            # final_output = mlp_model(output)
+            distance = F.pairwise_distance(output1.view(output1.shape[0], -1), output2.view(output1.shape[0], -1), keepdim=True)
+            if targets.item() == 1:
+                pos_distances.append(distance.item())
+            else:
+                neg_distances.append(distance.item())
+
+    data = [pos_distances, neg_distances]
+
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(8, 6))
+    sns.violinplot(data=data)
+    plt.xticks([0, 1], ['Forgery', 'Original'])
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    filename = f"/home/jamesemi/Desktop/james/adl/ViTPose_pytorch/figs/violin_plot_{timestamp}.pth"
+    plt.savefig('/home/jamesemi/Desktop/james/adl/ViTPose_pytorch/figs/violin_cocob_finetune.png', dpi=300)
+    return pos_distances, neg_distances
 
 
 def save_model(model, epoch, loss, descriptor="ViTPose"):
@@ -373,35 +418,38 @@ def main():
     model = ViTPose().to(device)
     mlp_model = MLPClassifier(input_dim=34*64*64, hidden_dim=512, output_dim=1).to(device) #update this
     # mlp_model = MLPClassifier(input_dim=34*64*64, hidden_dim=512, output_dim=1).to(device) #update this
-      
+    # model = torch.load('/home/nthumbav/Downloads/ViTPose_pytorch/vitpose_ckpt.pth')
     # Path to the pretrained model checkpoint
-    # checkpoint_path = '/notebooks/ADL/ViTPose_pytorch/chkpts/vitpose-b-multi-coco.pth'
-    # # checkpoint_path = '/ViTPose_pytorch/chkpts/xyz.pth'
-    # # checkpoint_path = None
+    checkpoint_path = '/data-fast/james/adl/chkpts/vitpose-b-multi-coco.pth'
+    # checkpoint_path = '/ViTPose_pytorch/chkpts/xyz.pth'
+    # checkpoint_path = None
+
+    num_epochs = 50
+    learning_rate = 1e-3
     
-    # if os.path.exists(checkpoint_path):
-    #     checkpoint = torch.load(checkpoint_path, map_location=device)
-    #     state_dict = checkpoint['state_dict']
+    if os.path.exists(checkpoint_path):
+        checkpoint = torch.load(checkpoint_path, map_location=device)
+        state_dict = checkpoint['state_dict']
 
-    #     # Adjust the first convolutional layer weights
-    #     # Average the RGB channels weights to fit grayscale input
-    #     first_conv_weight = state_dict['backbone.patch_embed.proj.weight']
-    #     first_conv_weight_mean = first_conv_weight.mean(dim=1, keepdim=True)
-    #     state_dict['backbone.patch_embed.proj.weight'] = first_conv_weight_mean
+        # Adjust the first convolutional layer weights
+        # Average the RGB channels weights to fit grayscale input
+        first_conv_weight = state_dict['backbone.patch_embed.proj.weight']
+        first_conv_weight_mean = first_conv_weight.mean(dim=1, keepdim=True)
+        state_dict['backbone.patch_embed.proj.weight'] = first_conv_weight_mean
 
-    #     # Resize positional embeddings if necessary
-    #     pos_embed = state_dict['backbone.pos_embed']
-    #     current_pos_embed = model.backbone.pos_embed
-    #     if pos_embed.shape != current_pos_embed.shape:
-    #         # Interpolate positional embeddings
-    #         new_pos_embed = F.interpolate(pos_embed.permute(0, 2, 1), size=current_pos_embed.shape[-2], mode='linear', align_corners=True)
-    #         state_dict['backbone.pos_embed'] = new_pos_embed.permute(0, 2, 1)
+        # Resize positional embeddings if necessary
+        pos_embed = state_dict['backbone.pos_embed']
+        current_pos_embed = model.backbone.pos_embed
+        if pos_embed.shape != current_pos_embed.shape:
+            # Interpolate positional embeddings
+            new_pos_embed = F.interpolate(pos_embed.permute(0, 2, 1), size=current_pos_embed.shape[-2], mode='linear', align_corners=True)
+            state_dict['backbone.pos_embed'] = new_pos_embed.permute(0, 2, 1)
 
-    #     # Load the adjusted state dict
-    #     model.load_state_dict(state_dict, strict=False)
-    #     print("Pretrained model loaded successfully with adjustments.")
-    # else:
-    #     print("Checkpoint file not found. Training from scratch.")
+        # Load the adjusted state dict
+        model.load_state_dict(state_dict, strict=False)
+        print("Pretrained model loaded successfully with adjustments.")
+    else:
+        print("Checkpoint file not found. Training from scratch.")
 
     
     # Load dataset
@@ -412,16 +460,16 @@ def main():
     # Initialize dataset
     # dataset = DupletDatasetCEDAR(base_dir='OSV_2D/CEDAR', transform=transform)
     dirs = defaultdict(list)
-    dirs['train'] = '/home/nthumbav/Downloads/OSV_2D/CEDAR/train'
-    dirs['val'] = '/home/nthumbav/Downloads/OSV_2D/CEDAR/val'
-    dirs['test'] = '/home/nthumbav/Downloads/OSV_2D/CEDAR/test'
+    dirs['train'] = '/home/jamesemi/Desktop/james/adl/ViTPose_pytorch/datasets/CEDAR/train'
+    dirs['val'] = '/home/jamesemi/Desktop/james/adl/ViTPose_pytorch/datasets/CEDAR/val'
+    dirs['test'] = '/home/jamesemi/Desktop/james/adl/ViTPose_pytorch/datasets/CEDAR/test'
     
     train_signers = [name for name in os.listdir(dirs['train']) if os.path.isdir(os.path.join(dirs['train'], name))]
     val_signers = [name for name in os.listdir(dirs['val']) if os.path.isdir(os.path.join(dirs['val'], name))]
     test_signers = [name for name in os.listdir(dirs['test']) if os.path.isdir(os.path.join(dirs['test'], name))]
     
     #COMMENT OUT THE FOLLOWING TEST
-    # train_signers, val_signers = train_signers[:2], val_signers[:1]
+    #train_signers, val_signers = train_signers[:2], val_signers[:1]
 
     train_dataset = DupletDatasetCEDAR(dirs['train'], transform=transform, signers=train_signers)
     val_dataset = DupletDatasetCEDAR(dirs['val'], transform=transform, signers=val_signers)
@@ -430,10 +478,16 @@ def main():
     # Load datasets
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
     
-    train_model(model, mlp_model, train_loader, val_loader, device, patience=10)
-    
+    #uncomment below for testing
+    # test_model(model, mlp_model, test_loader, device)
 
+    #uncomment below for training run
+    train_losses, val_losses = train_model(model, mlp_model, train_loader, val_loader, device, patience=5, lr=learning_rate, num_epochs=num_epochs)
+    plot_losses(train_losses, val_losses, learning_rate, num_epochs)
+
+
+    
 if __name__ == '__main__':
     main()
